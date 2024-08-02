45c433757f814660563467bbe967bdc5a61001df
Paul Knapp <ppankluap@gmail.com>
2024-08-01 22:06:17 -0500

    finish the example code

--- if_statements.rb
+++ if_statements.rb
@@ -41,12 +41,13 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 2
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
-
-
+if num_quarters >= 2
+  puts "I have enough money for a gumball"
+  elsif num_quarters < 2 
+    puts "I don't have enough money for a gumball"
+end
 #####################
 # Using the variables defined below, write code that will tell you
 # if you have the ingredients to make a pizza. A pizza requires 
@@ -61,5 +62,12 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
-has_sauce = true
+cups_of_flour = 200
+has_sauce = false
+
+if cups_of_flour >= 2 && has_sauce == true
+  puts "Pizza Time son"
+  elsif cups_of_flour < 2 || has_sauce != true
+    puts "I cant make pizza"
+    
+end
