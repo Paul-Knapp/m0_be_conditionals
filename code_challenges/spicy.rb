6b1742aa5226e9509eef177581af88ca9687d514
Paul Knapp <ppankluap@gmail.com>
2024-08-01 22:20:31 -0500

    Initial commit

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,11 @@
+input = gets
+
+if (input%3) == 0 && (input%5) == 0
+    puts "FizzBuzz"
+elsif (input%3) == 0 && (input%5) != 0
+    puts "Fizz"
+elsif (input%5) == 0 && (input%3) != 0
+    puts "Buzz"
+elsif (input%3) != 0 && (input%5) != 0
+    puts input
+end
